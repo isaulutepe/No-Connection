3ea2e1c0914e764c4c1918cbf1f9a2ba72679cd6
İsa <ulutepe42@gmail.com>
2023-10-23 00:35:10 +0300

    Added Animations

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,5 +6,6 @@ public class GameManager : MonoBehaviour
 {
 
     public bool isDead = false;
+    //Oyun baþlamadan önce 3 saniye geri sayým olacak ve buradan yönetilecek.
 
 }
--- Assets/Scripts/Player Scripts/Move.cs
+++ Assets/Scripts/Player Scripts/Move.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    [SerializeField]
     private float moveSpeed = 2f;
+    
+    //Sað - Sol sýnýrý almak için.
     public float border;
+
     private GameManager gameManager;
+
+    //Animasyon iþlemleri
+    public Animator animators; //Animasyonlarý yönetmek için.
+    private bool leftMove;
+    private bool rightMove;
+    private bool topMove;
+    private bool bottomMove;
+    private bool idle;
+
+
+
 
     private void Awake()
     {
@@ -20,14 +35,34 @@ public class Move : MonoBehaviour
     {
         if (gameManager.isDead == false)
         {
-            if (Input.GetKey(KeyCode.A) && transform.position.x > -border)
+            if (Input.GetKey(KeyCode.A) && transform.position.x > -border) //Sol
             {
                 transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+                rightMove= true;
             }
-            if (Input.GetKey(KeyCode.D) && transform.position.x < border)
+            if (Input.GetKey(KeyCode.D) && transform.position.x < border) //Sað
             {
                 transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+                leftMove= true;
             }
         }
+        //CheackAnimations();
+    }
+    void CheackAnimations()
+    {
+        if (rightMove == false || leftMove == false)
+        {
+            idle = true;
+        }
+        if (idle== true)
+        {
+            animators.SetBool("run", false);
+            animators.SetBool("goIdle", true);
+        }
+        if (rightMove == true || leftMove == true)
+        {
+            animators.SetBool("run", true);
+        }
+
     }
 }
